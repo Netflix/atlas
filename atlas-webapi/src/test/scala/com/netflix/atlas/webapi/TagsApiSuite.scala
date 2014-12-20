573660fc7fad883f47529b3b624515dd410a1c22
Brian Harrington <brharrington@gmail.com>
2014-12-20 12:47:29 -0800

    avoid repetition of uri

--- atlas-webapi/src/test/scala/com/netflix/atlas/webapi/TagsApiSuite.scala
+++ atlas-webapi/src/test/scala/com/netflix/atlas/webapi/TagsApiSuite.scala
@@ -29,99 +29,77 @@ class TagsApiSuite extends FunSuite with ScalatestRouteTest {
 
   val endpoint = new TagsApi
 
-  test("/api/v1/tags") {
-    Get("/api/v1/tags") ~> endpoint.routes ~> check {
-      assert(responseAs[String] === """["class","name","prime"]""")
+  def testGet(uri: String)(f: => Unit): Unit = {
+    test(uri) {
+      Get(uri) ~> endpoint.routes ~> check(f)
     }
   }
 
-  test("/api/v1/tags/") {
-    Get("/api/v1/tags/") ~> endpoint.routes ~> check {
-      assert(responseAs[String] === """["class","name","prime"]""")
-    }
+  testGet("/api/v1/tags") {
+    assert(responseAs[String] === """["class","name","prime"]""")
   }
 
-  test("/api/v1/tags?format=txt") {
-    Get("/api/v1/tags?format=txt") ~> endpoint.routes ~> check {
+  testGet("/api/v1/tags/") {
+    assert(responseAs[String] === """["class","name","prime"]""")
+  }
+
+  testGet("/api/v1/tags?format=txt") {
       assert(responseAs[String] === "class\nname\nprime")
-    }
   }
 
-  test("/api/v1/tags?limit=1") {
-    Get("/api/v1/tags?limit=1") ~> endpoint.routes ~> check {
-      assert(response.headers.exists(h => h.is(TagsApi.offsetHeader) && h.value == "class"))
-      assert(responseAs[String] === """["class"]""")
-    }
+  testGet("/api/v1/tags?limit=1") {
+    assert(response.headers.exists(h => h.is(TagsApi.offsetHeader) && h.value == "class"))
+    assert(responseAs[String] === """["class"]""")
   }
 
-  test("/api/v1/tags?limit=1&offset=class") {
-    Get("/api/v1/tags?limit=1&offset=class") ~> endpoint.routes ~> check {
-      assert(response.headers.exists(h => h.is(TagsApi.offsetHeader) && h.value == "name"))
-      assert(responseAs[String] === """["name"]""")
-    }
+  testGet("/api/v1/tags?limit=1&offset=class") {
+    assert(response.headers.exists(h => h.is(TagsApi.offsetHeader) && h.value == "name"))
+    assert(responseAs[String] === """["name"]""")
   }
 
-  test("/api/v1/tags?limit=1&offset=name") {
-    Get("/api/v1/tags?limit=1&offset=name") ~> endpoint.routes ~> check {
-      assert(response.headers.exists(h => h.is(TagsApi.offsetHeader) && h.value == "prime"))
-      assert(responseAs[String] === """["prime"]""")
-    }
+  testGet("/api/v1/tags?limit=1&offset=name") {
+    assert(response.headers.exists(h => h.is(TagsApi.offsetHeader) && h.value == "prime"))
+    assert(responseAs[String] === """["prime"]""")
   }
 
-  test("/api/v1/tags?limit=1&offset=prime") {
-    Get("/api/v1/tags?limit=1&offset=prime") ~> endpoint.routes ~> check {
-      assert(response.headers.forall(_.isNot(TagsApi.offsetHeader)))
-      assert(responseAs[String] === """[]""")
-    }
+  testGet("/api/v1/tags?limit=1&offset=prime") {
+    assert(response.headers.forall(_.isNot(TagsApi.offsetHeader)))
+    assert(responseAs[String] === """[]""")
   }
 
-  test("/api/v1/tags?limit=4&offset=class") {
-    Get("/api/v1/tags?limit=4&offset=class") ~> endpoint.routes ~> check {
-      assert(response.headers.forall(_.isNot(TagsApi.offsetHeader)))
-      assert(responseAs[String] === """["name","prime"]""")
-    }
+  testGet("/api/v1/tags?limit=4&offset=class") {
+    assert(response.headers.forall(_.isNot(TagsApi.offsetHeader)))
+    assert(responseAs[String] === """["name","prime"]""")
   }
 
-  test("/api/v1/tags?limit=foo") {
-    Get("/api/v1/tags?limit=foo") ~> endpoint.routes ~> check {
-      assert(response.status === StatusCodes.BadRequest)
-    }
+  testGet("/api/v1/tags?limit=foo") {
+    assert(response.status === StatusCodes.BadRequest)
   }
 
-  test("/api/v1/tags?verbose=1") {
-    Get("/api/v1/tags?verbose=1") ~> endpoint.routes ~> check {
-      val expected = Seq(toTagJson("class", "even", 6), toTagJson("class", "odd", 6)) ++
-        (0 to 11).map(toTagJson).sortWith(_ < _) ++
-        Seq(toTagJson("prime", "probably", 5))
-      assert(responseAs[String] === expected.mkString("[", ",", "]"))
-    }
+  testGet("/api/v1/tags?verbose=1") {
+    val expected = Seq(toTagJson("class", "even", 6), toTagJson("class", "odd", 6)) ++
+      (0 to 11).map(toTagJson).sortWith(_ < _) ++
+      Seq(toTagJson("prime", "probably", 5))
+    assert(responseAs[String] === expected.mkString("[", ",", "]"))
   }
 
-  test("/api/v1/tags/name") {
-    Get("/api/v1/tags/name") ~> endpoint.routes ~> check {
-      val expected = (0 to 11).map(i => f"$i%02d").mkString("[\"", "\",\"", "\"]")
-      assert(responseAs[String] === expected)
-    }
+  testGet("/api/v1/tags/name") {
+    val expected = (0 to 11).map(i => f"$i%02d").mkString("[\"", "\",\"", "\"]")
+    assert(responseAs[String] === expected)
   }
 
-  test("/api/v1/tags/name?q=name,01,:eq") {
-    Get("/api/v1/tags/name?q=name,01,:eq") ~> endpoint.routes ~> check {
-      assert(responseAs[String] === """["01"]""")
-    }
+  testGet("/api/v1/tags/name?q=name,01,:eq") {
+    assert(responseAs[String] === """["01"]""")
   }
 
-  test("/api/v1/tags/name?verbose=1") {
-    Get("/api/v1/tags/name?verbose=1") ~> endpoint.routes ~> check {
-      val expected = (0 to 11).map(toTagJson).mkString("[", ",", "]")
-      assert(responseAs[String] === expected)
-    }
+  testGet("/api/v1/tags/name?verbose=1") {
+    val expected = (0 to 11).map(toTagJson).mkString("[", ",", "]")
+    assert(responseAs[String] === expected)
   }
 
-  test("/api/v1/tags/name?verbose=1&format=txt") {
-    Get("/api/v1/tags/name?verbose=1&format=txt") ~> endpoint.routes ~> check {
-      val expected = (0 to 11).map(toTagText).mkString("\n")
-      assert(responseAs[String] === expected)
-    }
+  testGet("/api/v1/tags/name?verbose=1&format=txt") {
+    val expected = (0 to 11).map(toTagText).mkString("\n")
+    assert(responseAs[String] === expected)
   }
 
   private def toTagJson(v: Int): String = toTagJson("name", "%02d".format(v), 1)
