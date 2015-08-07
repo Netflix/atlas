06bf84dc4f3f0bd1e67f60c011ead1cc4d6588a9
Brian Harrington <brharrington@gmail.com>
2015-08-07 16:01:18 -0700

    update version selection logic

--- project/GitVersion.scala
+++ project/GitVersion.scala
@@ -4,15 +4,23 @@ import sbtrelease.Version
 import com.typesafe.sbt.SbtGit._
 
 object GitVersion {
+
+  // Base version for master branch
+  private val baseVersion = "v1.5.x"
+
   // 0.1.x
-  private val versionBranch = """v?([0-9\.]+?)(\.x)?""".r
+  private val versionBranch = """v?([0-9\.]+)(?:\.x)?""".r
 
   // v0.1.47-31-g230560c
   // v0.1.47-20150807.161518-9
-  private val snapshotVersion = """v?([0-9\.]+)-(\d+)-([0-9a-z]+)""".r
+  private val snapshotVersion = """v?([0-9\.]+)-(?:\d+)-(?:[0-9a-z]+)""".r
+
+  // 1.5.0-rc.1-123-gcbfe51a
+  private val candidateVersion = """v?([0-9\.]+)(?:-rc\.\d+)?-(?:\d+)-(?:[0-9a-z]+)""".r
 
   // v0.1.47
-  private val releaseVersion = """v?([0-9\.]+)""".r
+  // 1.5.0-rc.1
+  private val releaseVersion = """v?([0-9\.]+(?:-rc\.\d+)?)""".r
 
   /**
    * Needs to check for "false", don't assume it will ever be set to "true".
@@ -30,7 +38,7 @@ object GitVersion {
     val suffix = "-SNAPSHOT"
     branch match {
       case versionBranch(b) if !v2.startsWith(b) =>
-        s"${Version(b).map(_.string).getOrElse(v2)}$suffix"
+        s"${Version(s"$b.0").map(_.string).getOrElse(v2)}$suffix"
       case _ =>
         s"$v2$suffix"
     }
@@ -38,10 +46,13 @@ object GitVersion {
 
   lazy val settings: Seq[Def.Setting[_]] = Seq(
     version in ThisBuild := {
-      val branch = GitKeys.gitReader.value.withGit(_.branches).headOption.getOrElse("unknown")
+      val branch = git.gitCurrentBranch.value
+      val branchVersion = if (branch == "master") baseVersion else branch
+      println(s"here $branch, $branchVersion, ${git.gitDescribedVersion.value}")
       git.gitDescribedVersion.value getOrElse "0.1-SNAPSHOT" match {
         case _ if isPullRequest       => s"0.0.0-PULLREQUEST"
-        case snapshotVersion(v, _, _) => toSnapshotVersion(branch, v)
+        case snapshotVersion(v)       => toSnapshotVersion(branchVersion, v)
+        case candidateVersion(v)      => s"$v-SNAPSHOT"
         case releaseVersion(v)        => v
         case v                        => v
       }
