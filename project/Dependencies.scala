4b6c536f52ebe1a8b3041e5477e5dd857ca015e0
Brian Harrington <brharrington@gmail.com>
2016-02-03 13:06:08 -0800

    update to aws-java-sdk 1.10.50
    Adds support for g2.8xlarge type.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   object Versions {
     val akka       = "2.4.1"
-    val aws        = "1.10.49"
+    val aws        = "1.10.50"
     val iep        = "0.3.14"
     val guice      = "4.0"
     val jackson    = "2.6.3"
