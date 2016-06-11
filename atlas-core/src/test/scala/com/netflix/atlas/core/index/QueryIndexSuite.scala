0bc717bf70c234d7535bf9800acb8c5a2eea4fbe
Brian Harrington <brharrington@gmail.com>
2016-06-11 13:48:28 -0700

    intern query objects and strings while parsing
    The biggest chunk of memory usage for the test query set
    is coming from the query objects themselves. Sizes:
    
    ```
    Object                         Size (bytes)
    -------------------------------------------
    Input queries                        71,976
    Query index                          76,640
    Input queries w/ interning           22,896
    ```
    
    This is just an example as part of the test. It was needed
    so we could test larger data sets without getting an OOM
    exception. It is not being rolled into the query index
    because it would likely need to be done earlier while
    processing input queries. The mechanism can be copied from
    the test case for now if it is needed. If a more generic
    utility is found to be generally useful going forward,
    then we'll revisit.

--- atlas-core/src/test/scala/com/netflix/atlas/core/index/QueryIndexSuite.scala
+++ atlas-core/src/test/scala/com/netflix/atlas/core/index/QueryIndexSuite.scala
@@ -187,11 +187,47 @@ class QueryIndexSuite extends FunSuite {
     assert(Set(expr2) === index.matchingEntries(Map("name" -> "numCores")).toSet)
   }
 
-  private def parse(s: String): List[Query] = {
+  type QueryInterner = scala.collection.mutable.AnyRefMap[Query, Query]
+
+  private def intern(interner: QueryInterner, query: Query): Query = {
+    query match {
+      case Query.True =>
+        query
+      case Query.False =>
+        query
+      case q: Query.Equal =>
+        interner.getOrElseUpdate(q, Query.Equal(q.k.intern(), q.v.intern()))
+      case q: Query.LessThan =>
+        interner.getOrElseUpdate(q, Query.LessThan(q.k.intern(), q.v.intern()))
+      case q: Query.LessThanEqual =>
+        interner.getOrElseUpdate(q, Query.LessThanEqual(q.k.intern(), q.v.intern()))
+      case q: Query.GreaterThan =>
+        interner.getOrElseUpdate(q, Query.GreaterThan(q.k.intern(), q.v.intern()))
+      case q: Query.GreaterThanEqual =>
+        interner.getOrElseUpdate(q, Query.GreaterThanEqual(q.k.intern(), q.v.intern()))
+      case q: Query.Regex =>
+        interner.getOrElseUpdate(q, Query.Regex(q.k.intern(), q.v.intern()))
+      case q: Query.RegexIgnoreCase =>
+        interner.getOrElseUpdate(q, Query.RegexIgnoreCase(q.k.intern(), q.v.intern()))
+      case q: Query.In =>
+        interner.getOrElseUpdate(q, Query.In(q.k.intern(), q.vs.map(_.intern())))
+      case q: Query.HasKey =>
+        interner.getOrElseUpdate(q, Query.HasKey(q.k.intern()))
+      case q: Query.And =>
+        interner.getOrElseUpdate(q, Query.And(intern(interner, q.q1), intern(interner, q.q2)))
+      case q: Query.Or =>
+        interner.getOrElseUpdate(q, Query.Or(intern(interner, q.q1), intern(interner, q.q2)))
+      case q: Query.Not =>
+        interner.getOrElseUpdate(q, Query.Not(intern(interner, q.q)))
+    }
+  }
+
+  private def parse(interner: QueryInterner, s: String): List[Query] = {
     try {
       val interpreter = Interpreter(StyleVocabulary.allWords)
       val queries = interpreter.execute(s).stack.collect {
-        case ModelExtractors.PresentationType(t) => t.expr.dataExprs.map(_.query)
+        case ModelExtractors.PresentationType(t) =>
+          t.expr.dataExprs.map(e => intern(interner, e.query))
       }
       queries.flatten.distinct
     } catch {
@@ -199,8 +235,8 @@ class QueryIndexSuite extends FunSuite {
     }
   }
 
-
   ignore("memory") {
+    val interner = new QueryInterner
     val queries = Streams.scope(Streams.resource("queries.txt")) { in =>
       Streams.lines(in).toList.flatMap { u =>
         val uri = URI.create(u.replace("|", "%7C").replace("^", "%5E"))
@@ -208,7 +244,7 @@ class QueryIndexSuite extends FunSuite {
         if (qstring == null) Nil else {
           qstring.split("&")
             .filter(_.startsWith("q="))
-            .map(s => parse(s.substring(2)))
+            .map(s => parse(interner, s.substring(2)))
         }
       }
     }
