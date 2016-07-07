fbc12676ee66360cf14565e071354841687a608e
Brian Harrington <brharrington@gmail.com>
2016-07-06 20:07:59 -0700

    docs: update description for :ls

--- atlas-core/src/main/scala/com/netflix/atlas/core/model/StyleVocabulary.scala
+++ atlas-core/src/main/scala/com/netflix/atlas/core/model/StyleVocabulary.scala
@@ -132,10 +132,21 @@ object StyleVocabulary extends Vocabulary {
 
     override def summary: String =
       """
-        |Set the line style. The value should be one of `line`, `area`, `stack`, or `vspan`.
+        |Set the line style. The value should be one of:
+        |
+        |* `line`: this is the default, draws a normal line.
+        |* `area`: fill in the space between the line value and 0 on the Y-axis.
+        |* `stack`: stack the filled area on to the previous stacked lines on the same axis.
+        |* `vspan`: non-zero datapoints will be drawn as a vertical span.
+        |
+        |See the [line style examples](Line-Styles) page for more information.
       """.stripMargin.trim
 
-    override def examples: List[String] = List("name,sps,:eq,:sum,(,name,),:by,area")
+    override def examples: List[String] = List(
+      "name,sps,:eq,:sum,(,name,),:by,line",
+      "name,sps,:eq,:sum,(,name,),:by,area",
+      "name,sps,:eq,:sum,(,name,),:by,stack",
+      "name,sps,:eq,:sum,(,name,),:by,200e3,:gt,vspan")
   }
 
   case object LineWidth extends StyleWord {
