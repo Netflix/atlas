d909eac8e988a3d13500de8f2ff7dd1a454d0fdd
Michael Graff <mgraff@netflix.com>
2016-08-16 16:33:01 -0700

    tag messages with a uuid to avoid doing double work

--- atlas-lwcapi/src/main/scala/com/netflix/atlas/lwcapi/ExpressionDatabaseActor.scala
+++ atlas-lwcapi/src/main/scala/com/netflix/atlas/lwcapi/ExpressionDatabaseActor.scala
@@ -13,39 +13,44 @@ class ExpressionDatabaseActor(registry: Registry, channel: String) extends Actor
 
   private val subscriber = context.actorOf(Props(new Subscriber(subClient)))
 
+  val uuid = java.util.UUID.randomUUID.toString
+
   subscriber ! Register(redisCallback)
   val channels = Array("expressions")
   subscriber ! Subscribe(channels)
 
   def redisCallback(pubsub: PubSubMessage) = pubsub match {
+    // XXXMLG TODO should log
     case S(chan, cnt) => println("Subscribed to " + chan + " and count = " + cnt)
     case U(chan, cnt) => println("Unsubscribed to " + chan + " and count = " + cnt)
     case E(exc) => println("EXCEPTION: " + exc)
     case M(chan, msg) =>
-      val request = Json.decode[Request](msg)
-      println("RECEIVED:\n" + request)
-      request.action match {
-        case "add" => AlertMap.globalAlertMap.addExpr(request.expression)
-        case "delete" => AlertMap.globalAlertMap.delExpr(request.expression)
+      val request = Json.decode[RedisRequest](msg)
+      if (request.uuid != uuid) {
+        //println("RECEIVED:\n" + request)
+        request.action match {
+          case "add" => AlertMap.globalAlertMap.addExpr(request.expression)
+          case "delete" => AlertMap.globalAlertMap.delExpr(request.expression)
+        }
       }
   }
 
   def receive = {
     case Publish(expression) =>
       AlertMap.globalAlertMap.addExpr(expression)
-      val json = Json.encode(Publish(expression))
+      val json = Json.encode(RedisRequest(expression, uuid, "add"))
       pubClient.publish(channel, json)
     case Unpublish(expression) =>
       AlertMap.globalAlertMap.delExpr(expression)
-      val json = Json.encode(Unpublish(expression))
+      val json = Json.encode(RedisRequest(expression, uuid, "delete"))
       pubClient.publish(channel, json)
   }
 }
 
 object ExpressionDatabaseActor {
-  case class Request(expression: ExpressionWithFrequency, action: String)
-  case class Publish(expression: ExpressionWithFrequency) { val action = "add" }
-  case class Unpublish(expression: ExpressionWithFrequency) { val action = "delete" }
+  case class RedisRequest(expression: ExpressionWithFrequency, uuid: String, action: String)
+  case class Publish(expression: ExpressionWithFrequency)
+  case class Unpublish(expression: ExpressionWithFrequency)
 
   def props(registry: Registry, channel: String) = Props(new ExpressionDatabaseActor(registry, channel))
 }
