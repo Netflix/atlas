2f79b95866d77a2fa9238fd997b2ae662f42dbf0
Michael Graff <mgraff@netflix.com>
2016-08-24 10:11:09 -0700

    set my maven repo for builds

--- project/Bintray.scala
+++ project/Bintray.scala
@@ -18,7 +18,7 @@ object Bintray {
   lazy val settings: Seq[Def.Setting[_]] = bintraySettings ++ Seq(
     bintrayRepository := "maven",
     bintrayPackage := "atlas_v1",
-    bintrayOrganization := Some("netflixoss"),
+    bintrayOrganization := None,
     bintrayReleaseOnPublish := false,
     licenses += ("Apache-2.0", url("https://www.apache.org/licenses/LICENSE-2.0.txt")),
     credentials += Credentials("Artifactory Realm", "oss.jfrog.org", user, pass),
