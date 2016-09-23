5842ba400b9d7d98f747b68a8901b84ba6b028dd
Michael Graff <mgraff@netflix.com>
2016-09-23 10:18:31 -0700

    fix url

--- scripts/load-exprs-test.sh
+++ scripts/load-exprs-test.sh
@@ -1,1 +1,1 @@
-curl -X POST --data @exprs.json http://atlas-lwcapi.us-east-1.ieptest.netflix.net/lwc/api/v1/register
+curl -X POST --data @exprs.json http://atlas-lwcapi-iep.us-east-1.ieptest.netflix.net/lwc/api/v1/register
