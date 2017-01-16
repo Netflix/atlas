b8253a0e91d6460f81ec527ab727d9ca689ae647
Brian Harrington <brharrington@gmail.com>
2017-01-16 09:51:49 -0800

    frigga 0.17.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -27,7 +27,7 @@ object Dependencies {
   val awsS3           = "com.amazonaws" % "aws-java-sdk-s3" % aws
   val caffeine        = "com.github.ben-manes.caffeine" % "caffeine" % "2.3.3"
   val equalsVerifier  = "nl.jqno.equalsverifier" % "equalsverifier" % "2.1.8"
-  val frigga          = "com.netflix.frigga" % "frigga" % "0.15.0"
+  val frigga          = "com.netflix.frigga" % "frigga" % "0.17.0"
   val guiceCore       = "com.google.inject" % "guice" % guice
   val guiceMulti      = "com.google.inject.extensions" % "guice-multibindings" % guice
   val iepGuice        = "com.netflix.iep" % "iep-guice" % iep
