ec146caeeb0590b4f08f4a681371ba76fe87b0a5
Brian Harrington <brharrington@gmail.com>
2017-02-10 19:54:17 -0800

    fix #498, always honor order (#500)
    The order was getting ignored if there wasn't
    an explicit sort setting.

--- atlas-webapi/src/main/scala/com/netflix/atlas/webapi/GraphRequestActor.scala
+++ atlas-webapi/src/main/scala/com/netflix/atlas/webapi/GraphRequestActor.scala
@@ -172,7 +172,7 @@ class GraphRequestActor(registry: Registry) extends Actor with ActorLogging {
       lines: List[LineDef]): List[LineDef] = {
 
     import java.lang.{Double => JDouble}
-    sortBy.fold(lines) { mode =>
+    val sorted = sortBy.fold(lines) { mode =>
       val cmp: Function2[LineDef, LineDef, Boolean] = mode match {
         case "legend" => (a, b) => a.data.label < b.data.label
         case "min"    => (a, b) => JDouble.compare(a.legendStats.min, b.legendStats.min) < 0
@@ -185,9 +185,9 @@ class GraphRequestActor(registry: Registry) extends Actor with ActorLogging {
           warnings += s"Invalid sort mode '$order'. Using default of 'legend'."
           (a, b) => a.data.label < b.data.label
       }
-      val sorted = lines.sortWith(cmp)
-      if (useDescending) sorted.reverse else sorted
+      lines.sortWith(cmp)
     }
+    if (useDescending) sorted.reverse else sorted
   }
 }
 
