Netflix/atlas

commit fed398a808514abcf3172d68378980156d4d0da1

named rewrite expr string now preserves offset (#540)
Fixes a bug with the expression string for named rewrites
that contain offsets. If the rewrite is being used similar
to an aggregate function and the display expression is a
query, then the offset was getting lost because that is an
attribute of the aggregate functions that are only present
in the expanded evaluation expression.

Now the named rewrite will extract the offset and group by
information if the display type is a query. For others the
rewrite will be re-evaluated using the same interpreter.

atlas-core/src/main/scala/com/netflix/atlas/core/model/MathExpr.scala
@@ -15,11 +15,13 @@
  */
 package com.netflix.atlas.core.model
 
+import java.time.Duration
 import java.time.Instant
 import java.time.ZoneOffset
 import java.time.ZonedDateTime
 import java.time.temporal.ChronoField
 
+import com.netflix.atlas.core.stacklang.Context
 import com.netflix.atlas.core.util.ArrayHelper
 import com.netflix.atlas.core.util.Math
 import com.netflix.spectator.api.histogram.PercentileBuckets
@@ -563,10 +565,58 @@ object MathExpr {
     }
   }
 
-  case class NamedRewrite(name: String, displayExpr: Expr, evalExpr: TimeSeriesExpr)
+  /**
+    * Named rewrites are used to keep track of the user intent for operations and
+    * macros that are defined in terms of other basic operations. For example, `:avg`
+    * is not available as a basic aggregate type, it is a rewrite to
+    * `query,:sum,query,:count,:div`. However, for the user it is better if we can
+    * show `query,:avg` when dumping the expression as a string.
+    *
+    * @param name
+    *     Name of the operation, e.g., `avg`.
+    * @param displayExpr
+    *     Expression that is displayed to the user when creating the expression string.
+    * @param evalExpr
+    *     Expression that is evaluated.
+    * @param context
+    *     Evaluation context for the initial creation time. This context is used to
+    *     re-evaluate the rewrite using the original context if the overall expression
+    *     is rewritten ([[Expr.rewrite()]]) later.
+    */
+  case class NamedRewrite(
+    name: String,
+    displayExpr: Expr,
+    evalExpr: TimeSeriesExpr,
+    context: Context)
       extends TimeSeriesExpr {
+
     def dataExprs: List[DataExpr] = evalExpr.dataExprs
-    override def toString: String = s"$displayExpr,:$name"
+
+    override def toString: String = {
+      displayExpr match {
+        case q: Query =>
+          // If the displayExpr is a query type, then the rewrite is simulating an
+          // aggregate function. Modifications to the aggregate need to be represented
+          // after the operation as part of the expression string. There are two
+          // categories: offsets applied to the data function and group by.
+          val buffer = new StringBuilder
+          buffer.append(s"$q,:$name")
+
+          val offsets = evalExpr.dataExprs.map(_.offset).distinct
+          offsets match {
+            case d :: Nil if !d.isZero => buffer.append(s",$d,:offset")
+            case _                     =>
+          }
+
+          evalExpr.dataExprs
+            .collectFirst { case DataExpr.GroupBy(_, ks) => ks }
+            .foreach { ks => buffer.append(ks.mkString(",(,", ",", ",),:by")) }
+
+          buffer.toString()
+        case _ =>
+          s"$displayExpr,:$name"
+      }
+    }
 
     def isGrouped: Boolean = evalExpr.isGrouped
 
@@ -574,6 +624,28 @@ object MathExpr {
 
     def eval(context: EvalContext, data: Map[DataExpr, List[TimeSeries]]): ResultSet = {
       evalExpr.eval(context, data).copy(expr = this)
+    }
+
+    override def rewrite(f: PartialFunction[Expr, Expr]): Expr = {
+      if (f.isDefinedAt(this) && !f.isDefinedAt(displayExpr) && !f.isDefinedAt(evalExpr)) {
+        // The partial function is defined for the rewrite itself, assume the caller
+        // knows what to do
+        super.rewrite(f)
+      } else {
+        displayExpr match {
+          case q: Query =>
+            copy(
+              displayExpr = displayExpr.rewrite(f),
+              evalExpr = evalExpr.rewrite(f).asInstanceOf[TimeSeriesExpr])
+          case _ =>
+            val newDisplayExpr = displayExpr.rewrite(f)
+            val ctxt = context.interpreter.execute(toString)
+            ctxt.stack match {
+              case (r: NamedRewrite) :: Nil => r
+              case _ => throw new IllegalStateException(s"invalid stack for :$name")
+            }
+        }
+      }
     }
   }
 }

atlas-core/src/test/scala/com/netflix/atlas/core/model/MathExamplesSuite.scala
@@ -20,4 +20,35 @@ import com.netflix.atlas.core.stacklang.Vocabulary
 
 class MathExamplesSuite extends BaseExamplesSuite {
   override def vocabulary: Vocabulary = MathVocabulary
+
+  private def eval(program: String): TimeSeriesExpr = {
+    interpreter.execute(program).stack match {
+      case ModelExtractors.TimeSeriesType(t) :: Nil => t
+    }
+  }
+
+  test("toString with offsets") {
+    val expr = eval("name,test,:eq,:sum,1h,:offset")
+    assert(expr.toString === "name,test,:eq,:sum,PT1H,:offset")
+  }
+
+  test("rewrite toString with offsets") {
+    val expr = eval("name,test,:eq,:dist-avg,1h,:offset")
+    assert(expr.toString === "name,test,:eq,:dist-avg,PT1H,:offset")
+  }
+
+  test("rewrite toString with offsets and cq") {
+    val expr = eval("name,test,:eq,:avg,1h,:offset,app,foo,:eq,:cq")
+    assert(expr.toString === "name,test,:eq,app,foo,:eq,:and,:avg,PT1H,:offset")
+  }
+
+  test("rewrite group by toString with offsets") {
+    val expr = eval("name,test,:eq,:dist-avg,(,cluster,),:by,1h,:offset")
+    assert(expr.toString === "name,test,:eq,:dist-avg,PT1H,:offset,(,cluster,),:by")
+  }
+
+  test("rewrite group by toString with offsets and cq") {
+    val expr = eval("name,test,:eq,:dist-avg,(,cluster,),:by,1h,:offset,app,foo,:eq,:cq")
+    assert(expr.toString === "name,test,:eq,app,foo,:eq,:and,:dist-avg,PT1H,:offset,(,cluster,),:by")
+  }
 }

atlas-core/src/test/scala/com/netflix/atlas/core/stacklang/BaseExamplesSuite.scala
@@ -21,13 +21,13 @@ abstract class BaseExamplesSuite extends FunSuite {
 
   def vocabulary: Vocabulary
 
-  val i = new Interpreter(vocabulary.allWords)
+  protected val interpreter = new Interpreter(vocabulary.allWords)
 
   for (w <- vocabulary.words; ex <- w.examples) {
     if (ex.startsWith("UNK:")) {
       test(s"noException -- $ex,:${w.name}") {
         val prg = ex.substring("UNK:".length)
-        try i.execute(s"$prg,:${w.name}") catch {
+        try interpreter.execute(s"$prg,:${w.name}") catch {
           case e: IllegalArgumentException if e.getMessage.startsWith("unknown word ") =>
           case e: Exception => throw e
         }
@@ -35,21 +35,21 @@ abstract class BaseExamplesSuite extends FunSuite {
     } else if (ex.startsWith("ERROR:")) {
       test(s"exception -- $ex,:${w.name}") {
         val prg = ex.substring("ERROR:".length)
-        intercept[Exception] { i.execute(s"$prg,:${w.name}") }
+        intercept[Exception] { interpreter.execute(s"$prg,:${w.name}") }
       }
     } else {
       test(s"noException -- $ex,:${w.name}") {
-        i.execute(s"$ex,:${w.name}")
+        interpreter.execute(s"$ex,:${w.name}")
       }
 
       test(s"toString(item) -- $ex,:${w.name}") {
-        val stack = i.execute(s"$ex,:${w.name}").stack
+        val stack = interpreter.execute(s"$ex,:${w.name}").stack
         stack.foreach { item =>
           val prg = item match {
             case vs: List[_] => vs
             case v           => List(v)
           }
-          val stack2 = i.execute(Interpreter.toString(prg)).stack
+          val stack2 = interpreter.execute(Interpreter.toString(prg)).stack
           assert(stack2 === prg)
         }
       }
@@ -57,8 +57,8 @@ abstract class BaseExamplesSuite extends FunSuite {
       // Exclude offset because list form is lazily evaluated and breaks the comparison
       if (w.name != "offset") {
         test(s"toString(stack) -- $ex,:${w.name}") {
-          val stack = i.execute(s"$ex,:${w.name}").stack
-          assert(stack === i.execute(Interpreter.toString(stack)).stack)
+          val stack = interpreter.execute(s"$ex,:${w.name}").stack
+          assert(stack === interpreter.execute(Interpreter.toString(stack)).stack)
         }
       }
     }

atlas-core/src/main/scala/com/netflix/atlas/core/model/MathVocabulary.scala
@@ -17,6 +17,7 @@ package com.netflix.atlas.core.model
 
 import com.netflix.atlas.core.model.DataExpr.AggregateFunction
 import com.netflix.atlas.core.model.MathExpr.AggrMathExpr
+import com.netflix.atlas.core.stacklang.Context
 import com.netflix.atlas.core.stacklang.SimpleWord
 import com.netflix.atlas.core.stacklang.StandardVocabulary.Macro
 import com.netflix.atlas.core.stacklang.Vocabulary
@@ -57,7 +58,7 @@ object MathVocabulary extends Vocabulary {
         ":dup", ":sum", ":swap", ":count", ":div",
         "avg", ":named-rewrite"
       ),
-      List("name,sps,:eq,(,nf.cluster,),:by")),
+      List("name,sps,:eq,(,nf.cluster,),:by", "name,sps,:eq,1h,:offset")),
 
     Macro("stddev", List(
         // Copy of base query
@@ -191,17 +192,6 @@ object MathVocabulary extends Vocabulary {
       case StringListType(keys) :: TimeSeriesType(t) :: stack =>
         // Default data group by applied across math operations
         val f = t.rewrite {
-          case MathExpr.NamedRewrite(n, q: Query, t) =>
-            // A number of macro rewrites are helpers that are meant to look like
-            // aggregate functions to the user. These have an display type that is
-            // as simple query and an arbitrarily complicated eval expression. In
-            // those cases we rewrite the eval expression and generate a new name
-            // that indicates the group by operation over the simple query.
-            val evalExpr = t.rewrite {
-              case af: AggregateFunction => DataExpr.GroupBy(af, keys)
-            }
-            val name = s"$n,(,${keys.mkString(",")},),:by"
-            MathExpr.NamedRewrite(name, q, evalExpr.asInstanceOf[TimeSeriesExpr])
           case af: AggregateFunction => DataExpr.GroupBy(af, keys)
         }
         f :: stack
@@ -314,22 +304,37 @@ object MathVocabulary extends Vocabulary {
       "42,nf.app,alerttest,:eq")
   }
 
-  case object NamedRewrite extends SimpleWord {
+  case object NamedRewrite extends Word {
+
+    private type Rewrite = Expr => TimeSeriesExpr
+
     override def name: String = "named-rewrite"
+
+    def matches(stack: List[Any]): Boolean = {
+      if (matcher.isDefinedAt(stack)) matcher(stack) else false
+    }
+
+    def execute(context: Context): Context = {
+      val pf = executor(Context(context.interpreter, Nil, Map.empty))
+      if (pf.isDefinedAt(context.stack))
+        context.copy(stack = pf(context.stack))
+      else
+        invalidStack
+    }
 
     protected def matcher: PartialFunction[List[Any], Boolean] = {
       case (_: String) :: TimeSeriesType(_) :: TimeSeriesType(_) :: _ => true
     }
 
-    protected def executor: PartialFunction[List[Any], List[Any]] = {
+    protected def executor(context: Context): PartialFunction[List[Any], List[Any]] = {
       case (n: String) :: TimeSeriesType(rw) :: (orig: Expr) :: stack =>
         // If the original is already an expr type, e.g. a Query, then we should
         // preserve it without modification. So we first match for Expr.
-        MathExpr.NamedRewrite(n, orig, rw) :: stack
+        MathExpr.NamedRewrite(n, orig, rw, context) :: stack
       case (n: String) :: TimeSeriesType(rw) :: TimeSeriesType(orig) :: stack =>
         // This is a more general match that will coerce the original into a
         // TimeSeriesExpr if it is not one already, e.g., a constant.
-        MathExpr.NamedRewrite(n, orig, rw) :: stack
+        MathExpr.NamedRewrite(n, orig, rw, context) :: stack
     }
 
     override def summary: String =

atlas-webapi/src/main/scala/com/netflix/atlas/webapi/ExprApi.scala
@@ -207,8 +207,8 @@ object ExprApi {
     import MathExpr.NamedRewrite
     val cleaned = exprs.map { e =>
       val clean = e.rewrite {
-        case NamedRewrite(n, orig: Query,          _) => NamedRewrite(n, orig, DataExpr.Sum(orig))
-        case NamedRewrite(n, orig: TimeSeriesExpr, _) => NamedRewrite(n, orig, orig)
+        case r @ NamedRewrite(_, orig: Query,          _, _) => r.copy(evalExpr = DataExpr.Sum(orig))
+        case r @ NamedRewrite(_, orig: TimeSeriesExpr, _, _) => r.copy(evalExpr = orig)
       }
       clean.asInstanceOf[StyleExpr]
     }

commit b69d90af7f86533d388988b09850db070e69e507

fix rewrite to use new display expr
Fixes a regression introduced in #540. The non-query
case was re-evaluating the rewrite command on the
original query rather than the new rewritten display
expression.

atlas-core/src/main/scala/com/netflix/atlas/core/model/MathExpr.scala
@@ -592,8 +592,10 @@ object MathExpr {
 
     def dataExprs: List[DataExpr] = evalExpr.dataExprs
 
-    override def toString: String = {
-      displayExpr match {
+    override def toString: String = toString(displayExpr)
+
+    private def toString(expr: Expr): String = {
+      expr match {
         case q: Query =>
           // If the displayExpr is a query type, then the rewrite is simulating an
           // aggregate function. Modifications to the aggregate need to be represented
@@ -614,7 +616,7 @@ object MathExpr {
 
           buffer.toString()
         case _ =>
-          s"$displayExpr,:$name"
+          s"$expr,:$name"
       }
     }
 
@@ -639,7 +641,7 @@ object MathExpr {
               evalExpr = evalExpr.rewrite(f).asInstanceOf[TimeSeriesExpr])
           case _ =>
             val newDisplayExpr = displayExpr.rewrite(f)
-            val ctxt = context.interpreter.execute(toString)
+            val ctxt = context.interpreter.execute(toString(newDisplayExpr))
             ctxt.stack match {
               case (r: NamedRewrite) :: Nil => r
               case _ => throw new IllegalStateException(s"invalid stack for :$name")

atlas-core/src/test/scala/com/netflix/atlas/core/model/MathExamplesSuite.scala
@@ -21,12 +21,6 @@ import com.netflix.atlas.core.stacklang.Vocabulary
 class MathExamplesSuite extends BaseExamplesSuite {
   override def vocabulary: Vocabulary = MathVocabulary
 
-  private def eval(program: String): TimeSeriesExpr = {
-    interpreter.execute(program).stack match {
-      case ModelExtractors.TimeSeriesType(t) :: Nil => t
-    }
-  }
-
   test("toString with offsets") {
     val expr = eval("name,test,:eq,:sum,1h,:offset")
     assert(expr.toString === "name,test,:eq,:sum,PT1H,:offset")

atlas-core/src/test/scala/com/netflix/atlas/core/model/StatefulExamplesSuite.scala
@@ -20,4 +20,9 @@ import com.netflix.atlas.core.stacklang.Vocabulary
 
 class StatefulExamplesSuite extends BaseExamplesSuite {
   override def vocabulary: Vocabulary = StatefulVocabulary
+
+  test("rewrite toString and cq") {
+    val expr = eval("name,test,:eq,:sum,:des-fast,app,foo,:eq,:cq")
+    assert(expr.toString === "name,test,:eq,app,foo,:eq,:and,:sum,:des-fast")
+  }
 }

atlas-core/src/test/scala/com/netflix/atlas/core/stacklang/BaseExamplesSuite.scala
@@ -15,6 +15,8 @@
  */
 package com.netflix.atlas.core.stacklang
 
+import com.netflix.atlas.core.model.ModelExtractors
+import com.netflix.atlas.core.model.TimeSeriesExpr
 import org.scalatest.FunSuite
 
 abstract class BaseExamplesSuite extends FunSuite {
@@ -22,6 +24,12 @@ abstract class BaseExamplesSuite extends FunSuite {
   def vocabulary: Vocabulary
 
   protected val interpreter = new Interpreter(vocabulary.allWords)
+
+  protected def eval(program: String): TimeSeriesExpr = {
+    interpreter.execute(program).stack match {
+      case ModelExtractors.TimeSeriesType(t) :: Nil => t
+    }
+  }
 
   for (w <- vocabulary.words; ex <- w.examples) {
     if (ex.startsWith("UNK:")) {