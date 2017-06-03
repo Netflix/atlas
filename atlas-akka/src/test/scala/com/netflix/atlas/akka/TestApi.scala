99de91abac096045f1d6d468407804565afd0cea
Brian Harrington <brharrington@gmail.com>
2017-06-03 09:30:07 -0700

    fix deprecation warning for use of actor publisher
    There are a few more of these in other parts of the code,
    but they are a bit more involved to cleanup. Will look at
    those later.

--- atlas-akka/src/test/scala/com/netflix/atlas/akka/TestApi.scala
+++ atlas-akka/src/test/scala/com/netflix/atlas/akka/TestApi.scala
@@ -16,9 +16,9 @@
 package com.netflix.atlas.akka
 
 import akka.actor.ActorRefFactory
-import akka.actor.Props
 import akka.http.scaladsl.model.ContentTypes
 import akka.http.scaladsl.model.HttpEntity
+import akka.http.scaladsl.model.HttpEntity.ChunkStreamPart
 import akka.http.scaladsl.model.HttpResponse
 import akka.http.scaladsl.model.StatusCodes._
 import akka.http.scaladsl.server.Directives._
@@ -58,7 +58,9 @@ class TestApi(val actorRefFactory: ActorRefFactory) extends WebApi {
     accessLog {
       path("chunked") {
         get {
-          val source = Source.actorPublisher(Props(new ChunkResponseActor))
+            val source = Source
+              .single(ChunkStreamPart("start"))
+              .concat(Source((1 until 42).map(i => ChunkStreamPart(i.toString)).toList))
           val entity = HttpEntity.Chunked(ContentTypes.`text/plain(UTF-8)`, source)
           complete(HttpResponse(status = OK, entity = entity))
         }
