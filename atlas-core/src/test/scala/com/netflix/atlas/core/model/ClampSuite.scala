be4cd8b1b4899a3623167b66f08f7968df676cf3
brharrington <brharrington@gmail.com>
2017-06-20 11:05:17 -0700

    add :clamp-min and :clamp-max (#623)
    Allows the user to restrict the min and max values of
    the input respectively. The most common use-case motivating
    this is to be able to support a bounded auto-scaling for
    data on an axis. The axis lower and upper limits are either
    explicit or automatic. These operators give more flexiblity
    and can be set for a given line to tune its behavior.
    
    Name was chosen to match the same operators in Promoetheus.

---
+++ atlas-core/src/test/scala/com/netflix/atlas/core/model/ClampSuite.scala
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2014-2017 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.netflix.atlas.core.model
+
+import org.scalatest.FunSuite
+
+class ClampSuite extends FunSuite {
+
+  val step = 60000L
+  val dataTags = Map("name" -> "cpu", "node" -> "i-1")
+
+  val inputTS = TimeSeries(dataTags, new ArrayTimeSeq(DsType.Gauge, 0L, step,
+    Array[Double](1.0, 1.5, 1.6, 1.7, 1.4, 1.3, 1.2, 1.0, 0.0, 0.0)))
+
+  val des = StatefulExpr.Des(DataExpr.Sum(Query.Equal("name" , "cpu")), 2, 0.1, 0.02)
+
+  def eval(expr: TimeSeriesExpr, data: List[List[Datapoint]]): List[List[TimeSeries]] = {
+    var state = Map.empty[StatefulExpr, Any]
+    data.map { ts =>
+      val t = ts.head.timestamp
+      val context = EvalContext(t, t + step, step, state)
+      val rs = expr.eval(context, ts)
+      state = rs.state
+      rs.data
+    }
+  }
+
+  test("clamp-min") {
+    val s = 0L
+    val e = 10L * step
+    val context = EvalContext(s, e, step, Map.empty)
+    val clamp = MathExpr.ClampMin(DataExpr.Sum(Query.Equal("name" , "cpu")), 1.1)
+    val actual = clamp.eval(context, List(inputTS)).data.head.data.bounded(s, e).data
+    val expected = Array[Double](1.1, 1.5, 1.6, 1.7, 1.4, 1.3, 1.2, 1.1, 1.1, 1.1)
+    assert(actual === expected)
+  }
+
+  test("clamp-max") {
+    val s = 0L
+    val e = 10L * step
+    val context = EvalContext(s, e, step, Map.empty)
+    val clamp = MathExpr.ClampMax(DataExpr.Sum(Query.Equal("name" , "cpu")), 1.1)
+    val actual = clamp.eval(context, List(inputTS)).data.head.data.bounded(s, e).data
+    val expected = Array[Double](1.0, 1.1, 1.1, 1.1, 1.1, 1.1, 1.1, 1.0, 0.0, 0.0)
+    assert(actual === expected)
+  }
+
+}
