dbc44532a4d8fc3c7416bab796e6f59dfcbf3029
Brian Harrington <brharrington@gmail.com>
2017-07-04 09:50:42 -0700

    equalsverifier 2.3.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -30,7 +30,7 @@ object Dependencies {
   val awsEC2          = "com.amazonaws" % "aws-java-sdk-ec2" % aws
   val awsS3           = "com.amazonaws" % "aws-java-sdk-s3" % aws
   val caffeine        = "com.github.ben-manes.caffeine" % "caffeine" % "2.5.2"
-  val equalsVerifier  = "nl.jqno.equalsverifier" % "equalsverifier" % "2.3"
+  val equalsVerifier  = "nl.jqno.equalsverifier" % "equalsverifier" % "2.3.1"
   val frigga          = "com.netflix.frigga" % "frigga" % "0.18.0"
   val guiceCore       = "com.google.inject" % "guice" % guice
   val guiceMulti      = "com.google.inject.extensions" % "guice-multibindings" % guice
