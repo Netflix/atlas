0672badb98ffb93f2a8bf58c93fb432bd9c9b9b0
brharrington <brharrington@gmail.com>
2017-08-04 08:47:31 -0700

    disable eviction warnings for update task (#643)
    Trying to clean up some of the noise in the build
    logs. No one is paying attention to these warnings
    and if there is a real compatibility problem it
    should be caught in tests or the integration
    environment.

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -32,6 +32,9 @@ object BuildSettings {
     exportJars := true,   // Needed for one-jar, with multi-project
     externalResolvers := BuildSettings.resolvers,
 
+    // https://github.com/sbt/sbt/issues/1636
+    evictionWarningOptions in update := EvictionWarningOptions.empty,
+
     checkLicenseHeaders := License.checkLicenseHeaders(streams.value.log, sourceDirectory.value),
     formatLicenseHeaders := License.formatLicenseHeaders(streams.value.log, sourceDirectory.value),
 
