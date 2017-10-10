2ab4c0e19744b4332e05d854a289099b9a3d5fef
Brian Harrington <brharrington@gmail.com>
2017-10-10 09:19:17 -0700

    caffeine 2.5.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -31,7 +31,7 @@ object Dependencies {
   val awsCore         = "com.amazonaws" % "aws-java-sdk-core" % aws
   val awsEC2          = "com.amazonaws" % "aws-java-sdk-ec2" % aws
   val awsS3           = "com.amazonaws" % "aws-java-sdk-s3" % aws
-  val caffeine        = "com.github.ben-manes.caffeine" % "caffeine" % "2.5.5"
+  val caffeine        = "com.github.ben-manes.caffeine" % "caffeine" % "2.5.6"
   val equalsVerifier  = "nl.jqno.equalsverifier" % "equalsverifier" % "2.3.3"
   val frigga          = "com.netflix.frigga" % "frigga" % "0.18.0"
   val guiceCore       = "com.google.inject" % "guice" % guice
