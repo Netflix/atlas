095c91f0e5d26e907ef3f7c0c28554c16e20ce3b
brharrington <brharrington@gmail.com>
2017-10-11 17:51:03 -0700

    fix #695, enable build for jdk9 (#709)
    Update travis so we build on both jdk8 and jdk9.

--- scripts/buildViaTravis.sh
+++ scripts/buildViaTravis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This script will build the project.
 
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+if [ "$TRAVIS_PULL_REQUEST" != "false" ] || [ "$BINTRAY_PUBLISH" == "false" ]; then
   echo -e "Build Pull Request #$TRAVIS_PULL_REQUEST => Branch [$TRAVIS_BRANCH]"
   make build
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" == "" ]; then
