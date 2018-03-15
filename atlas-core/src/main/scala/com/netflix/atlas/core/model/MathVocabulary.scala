e27f20a46e48bfb2a161c56789388bac4d527836
Brian Harrington <brharrington@gmail.com>
2018-03-15 16:41:55 -0700

    docs: describe common use-case for clamp-max

--- atlas-core/src/main/scala/com/netflix/atlas/core/model/MathVocabulary.scala
+++ atlas-core/src/main/scala/com/netflix/atlas/core/model/MathVocabulary.scala
@@ -492,6 +492,7 @@ object MathVocabulary extends Vocabulary {
         |Restricts the minimum value of the output time series to the specified value. Values
         |from the input time series that are greater than or equal to the minimum will not be
         |changed. A common use-case is to allow for auto-scaled axis up to a specified bound.
+        |For more details see [:clamp-max](math-clamp‐max).
       """.stripMargin.trim
 
     override def signature: String = "TimeSeriesExpr Double -- TimeSeriesExpr"
@@ -518,7 +519,30 @@ object MathVocabulary extends Vocabulary {
       """
         |Restricts the maximum value of the output time series to the specified value. Values
         |from the input time series that are less than or equal to the maximum will not be
-        |changed. A common use-case is to allow for auto-scaled axis up to a specified bound.
+        |changed.
+        |
+        |A common use-case is to allow for auto-scaled axis up to a specified bound. The
+        |axis parameters for controlling the [axis bounds](Axis-Bounds) have the following
+        |limitations:
+        |
+        |- They apply to everything on the axis and cannot be targeted to a specific line.
+        |- Are either absolute or set based on the data. For data with occasional spikes
+        |  this can hide important details.
+        |
+        |Consider the following graph:
+        |
+        |![Spike](images/clamp-max-spike.png)
+        |
+        |The spike makes it difficult to make out any detail for other times. One option
+        |to handle this is to use an alternate [axis scale](Axis-Scale) such as
+        |[logarithmic](Axis-Scale#logarithmic) that gives a higher visual weight to the smaller
+        |values. However, it is often easier for a user to reason about a linear scale, in
+        |particular, for times when there is no spike in the graph window. If there is a known
+        |max reasonable value, then the `:clamp-max` operator can be used to restrict the line
+        |if and only if it exceeds the designated max. For example, if we limit the graph above
+        |to 25:
+        |
+        |![Spike with 25,:clamp-max](images/clamp-max-25.png)
       """.stripMargin.trim
 
     override def signature: String = "TimeSeriesExpr Double -- TimeSeriesExpr"
