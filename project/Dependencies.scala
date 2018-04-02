7d33e58fa61928adab684c507745e4fcde2ec8b2
Brian Harrington <brharrington@gmail.com>
2018-04-02 14:25:46 -0700

    typesafe config 1.3.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -70,7 +70,7 @@ object Dependencies {
   val spectatorLog4j  = "com.netflix.spectator" % "spectator-ext-log4j2" % spectator
   val spectatorM2     = "com.netflix.spectator" % "spectator-reg-metrics2" % spectator
   val spectatorSandbox= "com.netflix.spectator" % "spectator-ext-sandbox" % spectator
-  val typesafeConfig  = "com.typesafe" % "config" % "1.3.2"
+  val typesafeConfig  = "com.typesafe" % "config" % "1.3.3"
 }
 
 // format: on
