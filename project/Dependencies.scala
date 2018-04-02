d1009fdf00fde55db0899b049b831f174579de66
Brian Harrington <brharrington@gmail.com>
2018-04-02 14:34:36 -0700

    roaringbitmap 0.7.9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -56,7 +56,7 @@ object Dependencies {
   val log4jJcl        = "org.apache.logging.log4j" % "log4j-jcl" % log4j
   val log4jJul        = "org.apache.logging.log4j" % "log4j-jul" % log4j
   val log4jSlf4j      = "org.apache.logging.log4j" % "log4j-slf4j-impl" % log4j
-  val roaringBitmap   = "org.roaringbitmap" % "RoaringBitmap" % "0.7.0"
+  val roaringBitmap   = "org.roaringbitmap" % "RoaringBitmap" % "0.7.9"
   val scalaCompiler   = "org.scala-lang" % "scala-compiler"
   val scalaLibrary    = "org.scala-lang" % "scala-library"
   val scalaLibraryAll = "org.scala-lang" % "scala-library-all"
