2adacddb852ae2dc2c9418dc2307940998103f8a
Brian Harrington <brharrington@gmail.com>
2018-06-18 09:48:17 -0700

    spectator 0.70.2
    Minor change needed for test case because spectator
    will no longer force gauge updates inline. For more
    information see Netflix/spectator#562.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
     val log4j      = "2.11.0"
     val scala      = "2.12.6"
     val slf4j      = "1.7.25"
-    val spectator  = "0.68.0"
+    val spectator  = "0.70.2"
 
     val crossScala = Seq(scala, "2.11.12")
   }
