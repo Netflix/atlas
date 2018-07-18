da894a10edf28e409518276602019d6ceebae5d7
Brian Harrington <brharrington@gmail.com>
2018-07-18 06:40:02 -0700

    akka 2.5.14
    Adds support for async DNS resolver.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ import sbt._
 
 object Dependencies {
   object Versions {
-    val akka       = "2.5.13"
+    val akka       = "2.5.14"
     val akkaHttpV  = "10.1.3"
     val aws        = "1.11.339"
     val iep        = "1.2.3"
