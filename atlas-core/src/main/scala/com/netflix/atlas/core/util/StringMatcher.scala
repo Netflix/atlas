28a40ceb72a4f7a3c0a1123a3f34ccae201e51b7
brharrington <brharrington@gmail.com>
2018-07-24 11:18:11 -0700

    short circuit pattern matching with prefix check (#858)
    In the streaming execution, up to 25% of the CPU usage is
    checking regular expressions. This change improves the
    chances that checks will be able to quickly fail without
    needing regex by always checking the fixed prefix when
    known in the matches function. This wasn't done before
    because when evaluating with the index we already know
    the prefix matches before checking.

--- atlas-core/src/main/scala/com/netflix/atlas/core/util/StringMatcher.scala
+++ atlas-core/src/main/scala/com/netflix/atlas/core/util/StringMatcher.scala
@@ -47,6 +47,19 @@ object StringMatcher {
   /** A regex that has a simple prefix along with some arbitrary pattern. */
   private val PrefixPattern = """^\^+([\-_a-zA-Z0-9]+).*$""".r
 
+  /**
+    * Convert the string pattern into a matcher object. If possible, it will use a simple
+    * string matcher and avoid expensive regex operations.
+    *
+    * @param pattern
+    *     Regular expression string to compile.
+    * @param caseSensitive
+    *     Whether or not the match should be case sensitive. The default is true. Case
+    *     insensitive matching is more expensive and breaks the ability to reliably match
+    *     the prefix of queries. Avoid if possible.
+    * @return
+    *     Matcher based on the provided pattern.
+    */
   def compile(pattern: String, caseSensitive: Boolean = true): StringMatcher = {
     pattern match {
       case EqualsPattern(p) if caseSensitive      => Equals(p)
@@ -59,18 +72,18 @@ object StringMatcher {
       case AnchoredOrPattern(s) if caseSensitive  => compileOr(s, v => s"^$v$$")
       case StartOrPattern(s) if caseSensitive     => compileOr(s, v => s"^$v")
       case FloatingOrPattern(s) if caseSensitive  => compileOr(s, v => s"$v")
-      case PrefixPattern(p) if caseSensitive      => Regex(Some(p), Pattern.compile(pattern))
+      case PrefixPattern(p) if caseSensitive      => PrefixedRegex(p, Pattern.compile(pattern))
       case _                                      => default(pattern, caseSensitive)
     }
   }
 
   private def compileOr(s: String, f: String => String): StringMatcher = {
-    Or(s.split("\\|").toList.map(v => compile(f(v), true)))
+    Or(s.split("\\|").toList.map(v => compile(f(v))))
   }
 
-  private def default(re: String, caseSensitive: Boolean): StringMatcher = {
+  private def default(re: String, caseSensitive: Boolean = true): StringMatcher = {
     val flags = if (caseSensitive) 0 else Pattern.CASE_INSENSITIVE
-    Regex(None, Pattern.compile(re, flags))
+    Regex(Pattern.compile(re, flags))
   }
 
   /** Match all strings. */
@@ -80,7 +93,41 @@ object StringMatcher {
     def matches(v: String): Boolean = true
   }
 
-  case class Regex(prefix: Option[String], pattern: Pattern) extends StringMatcher {
+  case class PrefixedRegex(prefixStr: String, pattern: Pattern) extends StringMatcher {
+
+    val prefix: Option[String] = Some(prefixStr)
+
+    // Reuse matcher to reduce allocations, keep in thread local in-case this Regex instance is
+    // shared across multiple threads.
+    private val matcher = new ThreadLocal[Matcher] {
+
+      override protected def initialValue: Matcher = pattern.matcher("")
+    }
+
+    private def reMatches(v: String): Boolean = {
+      val m = matcher.get
+      m.reset(v)
+      m.find
+    }
+
+    def matches(v: String): Boolean = {
+      // Check with startsWith first to avoid using regex as much as possible. When using
+      // for streaming evaluation, this is a big win. When used with the index, we already
+      // know the prefix matches so it is a bit of extra overhead, but not too bad.
+      v.startsWith(prefixStr) && reMatches(v)
+    }
+
+    override def equals(obj: Any): Boolean = {
+      obj match {
+        case PrefixedRegex(p, re) =>
+          prefixStr == p && pattern.pattern() == re.pattern() && pattern.flags() == re.flags()
+        case _ => false
+      }
+    }
+  }
+
+  case class Regex(pattern: Pattern) extends StringMatcher {
+    val prefix: Option[String] = None
 
     // Reuse matcher to reduce allocations, keep in thread local in-case this Regex instance is
     // shared across multiple threads.
@@ -97,8 +144,8 @@ object StringMatcher {
 
     override def equals(obj: Any): Boolean = {
       obj match {
-        case Regex(p, re) =>
-          prefix == p && pattern.pattern() == re.pattern() && pattern.flags() == re.flags()
+        case Regex(re) =>
+          pattern.pattern() == re.pattern() && pattern.flags() == re.flags()
         case _ => false
       }
     }
@@ -140,6 +187,12 @@ object StringMatcher {
       }
       false
     }
+  }
+
+  case class And(matchers: List[StringMatcher]) extends StringMatcher {
+    val prefix: Option[String] = matchers.head.prefix
+
+    def matches(v: String): Boolean = matchers.forall(_.matches(v))
   }
 
   case class Or(matchers: List[StringMatcher]) extends StringMatcher {
