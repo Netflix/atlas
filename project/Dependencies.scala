5e21cef804c97d72245c9895d06f4ebb2314991a
Brian Harrington <brharrington@gmail.com>
2018-08-16 13:21:34 -0700

    spectator 0.74.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
     val log4j      = "2.11.0"
     val scala      = "2.12.6"
     val slf4j      = "1.7.25"
-    val spectator  = "0.72.0"
+    val spectator  = "0.74.2"
 
     val crossScala = Seq(scala, "2.11.12")
   }
