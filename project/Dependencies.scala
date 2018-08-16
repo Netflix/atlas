3267e1bdf8ef954c650fc8c00e094b881bf34ae9
Brian Harrington <brharrington@gmail.com>
2018-08-16 13:42:24 -0700

    aws-java-sdk 1.11.387

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   object Versions {
     val akka       = "2.5.14"
     val akkaHttpV  = "10.1.3"
-    val aws        = "1.11.356"
+    val aws        = "1.11.387"
     val iep        = "1.2.6"
     val guice      = "4.1.0"
     val jackson    = "2.9.6"
