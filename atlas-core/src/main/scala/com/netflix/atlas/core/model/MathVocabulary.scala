2c56abe4c01f9eca00812fb0941e2e7df6433801
Nathan Fisher <mail@nathfisher.com>
2018-09-24 13:03:05 -0700

    Add note about `$percentile` to percentiles description (#914)

--- atlas-core/src/main/scala/com/netflix/atlas/core/model/MathVocabulary.scala
+++ atlas-core/src/main/scala/com/netflix/atlas/core/model/MathVocabulary.scala
@@ -1085,6 +1085,8 @@ object MathVocabulary extends Vocabulary {
         |[PercentileDistributionSummary](http://netflix.github.io/spectator/en/latest/javadoc/spectator-api/com/netflix/spectator/api/histogram/PercentileDistributionSummary.html)
         |helper classes.
         |
+        |The percentile values can be shown in the legend using `$percentile`.
+        |
         |Since: 1.5.0 (first in 1.5.0-rc.4)
       """.stripMargin.trim
 
