a0bfbaa38424cedda2a88afded5d550d1540cf22
brharrington <brharrington@gmail.com>
2018-09-27 06:49:41 -0700

    update to scala 2.12.7 (#922)
    It is supposed to improve compiler performance.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
     val guice      = "4.1.0"
     val jackson    = "2.9.7"
     val log4j      = "2.11.1"
-    val scala      = "2.12.6"
+    val scala      = "2.12.7"
     val slf4j      = "1.7.25"
     val spectator  = "0.76.0"
 
