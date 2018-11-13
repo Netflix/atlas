3a081a0d54ec3941176cca39ec08c4347d823ad9
Brian Harrington <brharrington@gmail.com>
2018-11-13 07:28:37 -0800

    fix formatting

--- atlas-eval/src/test/scala/com/netflix/atlas/eval/stream/LwcToAggrDatapointSuite.scala
+++ atlas-eval/src/test/scala/com/netflix/atlas/eval/stream/LwcToAggrDatapointSuite.scala
@@ -132,7 +132,8 @@ class LwcToAggrDatapointSuite extends FunSuite {
   }
 
   test("invalid message") {
-    val msg = """data: metric {"timestamp":20000,"id":"sum","tags":{"name":"cpu"},\u007F"value":3.0}"""
+    val msg =
+      """data: metric {"timestamp":20000,"id":"sum","tags":{"name":"cpu"},\u007F"value":3.0}"""
     val results = eval(List(msg))
     assert(results.size === 0)
   }
