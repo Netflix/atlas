fee35775f089bfc274351a33eca6a9d5d2a36e5b
Brian Harrington <brharrington@gmail.com>
2018-12-20 12:41:12 -0800

    update to jackson 2.9.8
    Has a number of security fixes:
    
    https://groups.google.com/forum/#!topic/jackson-user/8jdpNS1dQPQ
    https://github.com/FasterXML/jackson/wiki/Jackson-Release-2.9.8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     val aws        = "1.11.469"
     val iep        = "1.2.9"
     val guice      = "4.1.0"
-    val jackson    = "2.9.7"
+    val jackson    = "2.9.8"
     val log4j      = "2.11.1"
     val scala      = "2.12.8"
     val slf4j      = "1.7.25"
