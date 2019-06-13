a51325d90817af3fe1e76b9e0c45dfae71c18a61
Brian Harrington <brharrington@gmail.com>
2019-06-13 10:14:14 -0700

    improve checks for recent jdk versions

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -11,7 +11,7 @@ object BuildSettings {
     "-feature"
   )
 
-  private val isJdk11 = System.getProperty("java.specification.version") == "11"
+  private val isRecentJdk = System.getProperty("java.specification.version").toDouble >= 11.0
 
   lazy val checkLicenseHeaders = taskKey[Unit]("Check the license headers for all source files.")
   lazy val formatLicenseHeaders = taskKey[Unit]("Fix the license headers for all source files.")
@@ -28,12 +28,12 @@ object BuildSettings {
       // -release option is not supported in scala 2.11
       val v = scalaVersion.value
       CrossVersion.partialVersion(v).map(_._2.toInt) match {
-        case Some(v) if v > 11 && isJdk11 => compilerFlags ++ Seq("-release", "8")
-        case _                            => compilerFlags ++ Seq("-target:jvm-1.8")
+        case Some(v) if v > 11 && isRecentJdk => compilerFlags ++ Seq("-release", "8")
+        case _                                => compilerFlags ++ Seq("-target:jvm-1.8")
       }
     },
     javacOptions ++= {
-      if (isJdk11)
+      if (isRecentJdk)
         Seq("--release", "8")
       else
         Seq("-source", "1.8", "-target", "1.8")
