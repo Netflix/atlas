2890e21cceb3f2d532fc84a4e501896674330327
Matt Zimmer <zimmermatt@netflix.com>
2019-08-05 11:30:48 -0700

    iep 2.1.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
     val akka       = "2.5.23"
     val akkaHttpV  = "10.1.8"
     val aws        = "1.11.579"
-    val iep        = "2.1.0"
+    val iep        = "2.1.1"
     val guice      = "4.1.0"
     val jackson    = "2.9.9"
     val log4j      = "2.12.0"
