7c6eb7f81227245fd65ec84477279f0f9254bc86
Tim Jiang <tjiang@netflix.com>
2019-12-23 06:30:37 -0800

    allow updating subscriptions via web socket API (#1118)
    Before it would work for the initial subscription, but didn't properly
    handle updates. This change adds a session manager that will handle
    the updated subscriptions.

---
+++ atlas-lwcapi/src/test/scala/com/netflix/atlas/lwcapi/WebSocketSessionManagerSuite.scala
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2014-2019 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.netflix.atlas.lwcapi
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.model.ws.Message
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.Sink
+import akka.stream.scaladsl.Source
+import com.netflix.atlas.json.JsonSupport
+import com.netflix.atlas.lwcapi.SubscribeApi.ErrorMsg
+import org.scalatest.funsuite.AnyFunSuite
+
+import scala.collection.mutable.ArrayBuffer
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+
+class WebSocketSessionManagerSuite extends AnyFunSuite {
+
+  private implicit val system = ActorSystem(getClass.getSimpleName)
+  private implicit val materializer = ActorMaterializer()
+
+  test("subscribe - one by one") {
+    val subscriptionList = List(
+      """[{"expression": "name,a,:eq,:sum", "step": 10000}]""",
+      """[{"expression": "name,b,:eq", "step": 5000}]"""
+    )
+
+    val subsCollector = ArrayBuffer[List[ExpressionMetadata]]()
+    val subFunc = createSubscribeFunc(subsCollector)
+    val regFun = createNoopRegisterFunc()
+
+    run(subscriptionList, regFun, subFunc)
+
+    assert(
+      subsCollector.toList === List(
+          List(ExpressionMetadata("name,a,:eq,:sum", 10000)),
+          List(ExpressionMetadata("name,b,:eq", 5000))
+        )
+    )
+  }
+
+  test("subscribe - ignore bad subscription") {
+    val subscriptionList = List(
+      """Bad Expression""",
+      """[{"expression": "name,b,:eq", "step": 5000}]"""
+    )
+    val subsCollector = ArrayBuffer[List[ExpressionMetadata]]()
+    val subFunc = createSubscribeFunc(subsCollector)
+    val regFun = createNoopRegisterFunc()
+
+    run(subscriptionList, regFun, subFunc)
+
+    assert(
+      subsCollector.toList === List(
+          List(ExpressionMetadata("name,b,:eq", 5000))
+        )
+    )
+  }
+
+  private def run(
+    data: List[String],
+    registerFunc: String => (QueueHandler, Source[Message, Unit]),
+    subscribeFunc: (String, List[ExpressionMetadata]) => List[ErrorMsg]
+  ): List[String] = {
+    val future = Source(data)
+      .via(new WebSocketSessionManager("", registerFunc, subscribeFunc))
+      .flatMapMerge(Int.MaxValue, source => source)
+      .map(_.asTextMessage.getStrictText)
+      .runWith(Sink.seq)
+
+    Await.result(future, Duration.Inf).toList
+  }
+
+  private def createSubscribeFunc(
+    subsCollector: ArrayBuffer[List[ExpressionMetadata]]
+  ): (String, List[ExpressionMetadata]) => List[ErrorMsg] = {
+    val subFunc = (_: String, expressions: List[ExpressionMetadata]) => {
+      subsCollector += expressions
+      List[ErrorMsg]()
+    }
+    subFunc
+  }
+
+  private def createNoopRegisterFunc(): String => (QueueHandler, Source[Message, Unit]) = {
+    val noopQueueHandler = new QueueHandler("", null) {
+      override def offer(msg: JsonSupport): Boolean = true
+      override def complete(): Unit = ()
+    }
+    val noopSource: Source[Message, Unit] = Source.empty[Message].mapMaterializedValue(_ => ())
+    val noopRegisterFunc = (_: String) => (noopQueueHandler, noopSource)
+
+    noopRegisterFunc
+  }
+}
