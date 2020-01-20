2ca176d8b81444b8dae9e72f47e88371c64766e4
Brian Harrington <brharrington@gmail.com>
2020-01-20 10:53:50 -0800

    slf4j 1.7.30

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
     val jackson    = "2.10.2"
     val log4j      = "2.12.1"
     val scala      = "2.13.1"
-    val slf4j      = "1.7.29"
+    val slf4j      = "1.7.30"
     val spectator  = "0.101.0"
 
     val crossScala = Seq(scala, "2.12.10")
