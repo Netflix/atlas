685c6dbacb1f7476a1228455869340f783966315
Brian Harrington <brharrington@gmail.com>
2020-01-20 12:08:48 -0800

    akka 2.5.27

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ import sbt._
 
 object Dependencies {
   object Versions {
-    val akka       = "2.5.26"
+    val akka       = "2.5.27"
     val akkaHttpV  = "10.1.11"
     val aws        = "1.11.702"
     val iep        = "2.1.6"
