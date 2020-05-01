15e41198f760f5baaba53eef6c689204b283dba0
Brian Harrington <brharrington@gmail.com>
2020-05-01 11:12:34 -0500

    spectator 0.107.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
     val log4j      = "2.13.1"
     val scala      = "2.13.2"
     val slf4j      = "1.7.30"
-    val spectator  = "0.104.0"
+    val spectator  = "0.107.0"
 
     val crossScala = Seq(scala)
   }
