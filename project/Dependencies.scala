7956d220c591931c682019919acbd21b0faba856
Brian Harrington <brharrington@gmail.com>
2020-05-01 10:54:55 -0500

    iep 2.5.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
     val akka       = "2.5.31"
     val akkaHttpV  = "10.1.11"
     val aws        = "1.11.764"
-    val iep        = "2.2.2"
+    val iep        = "2.5.0"
     val guice      = "4.1.0"
     val jackson    = "2.10.3"
     val log4j      = "2.13.1"
