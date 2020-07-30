dc6c35948ac25e13969074345055030db15edb09
Brian Harrington <brharrington@gmail.com>
2020-07-30 13:17:13 -0500

    jackson 2.11.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     val aws        = "1.11.790"
     val iep        = "2.5.5"
     val guice      = "4.1.0"
-    val jackson    = "2.11.0"
+    val jackson    = "2.11.1"
     val log4j      = "2.13.3"
     val scala      = "2.13.3"
     val slf4j      = "1.7.30"
