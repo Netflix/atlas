d2bfe586128235c77f0acc2771608ceb05d0346f
Brian Harrington <brharrington@gmail.com>
2020-10-09 12:01:04 -0500

    scala-collection-compat 2.2.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -61,7 +61,7 @@ object Dependencies {
   val log4jJul          = "org.apache.logging.log4j" % "log4j-jul" % log4j
   val log4jSlf4j        = "org.apache.logging.log4j" % "log4j-slf4j-impl" % log4j
   val roaringBitmap     = "org.roaringbitmap" % "RoaringBitmap" % "0.9.1"
-  val scalaCompat       = "org.scala-lang.modules" %% "scala-collection-compat" % "2.1.6"
+  val scalaCompat       = "org.scala-lang.modules" %% "scala-collection-compat" % "2.2.0"
   val scalaCompiler     = "org.scala-lang" % "scala-compiler"
   val scalaLibrary      = "org.scala-lang" % "scala-library"
   val scalaLibraryAll   = "org.scala-lang" % "scala-library-all"
