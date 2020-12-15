eba4b654ff405df4c27a44d9ca5b3e3a42478b95
Tim Jiang <tjiang@netflix.com>
2020-12-15 07:36:50 -0800

    add cardinality estimator, based on datasketches cpc (#1234)
    Based on Apache data sketches library. Will be used for detecting
    high cardinality dimensions for the purpose of automatic rollups.
    
    Co-authored-by: Brian Harrington <brharrington@gmail.com>

---
+++ atlas-core/src/main/scala/com/netflix/atlas/core/util/CardinalityEstimator.scala
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2014-2020 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.netflix.atlas.core.util
+
+import org.apache.datasketches.cpc.CpcSketch
+
+/**
+  * Utility for cheaply estimating the number of distinct values for a set of objects.
+  */
+trait CardinalityEstimator {
+
+  /**
+    * Update the estimate with the provided value. The object should have a good hash code
+    * implementation that is likely to be distinct for different values of the object.
+    */
+  def update(obj: AnyRef): Unit
+
+  /** Return the current estimate for the number of distinct values seen. */
+  def cardinality: Int
+}
+
+object CardinalityEstimator {
+
+  /**
+    * Create a new estimator instance using the [CPC] algorithm.
+    *
+    * [CPC]: https://datasketches.apache.org/docs/CPC/CPC.html
+    *
+    * @param lgK
+    *     Higher lgK means higher accuracy but higher space (`2^lgK` bytes), can be in range
+    *     of [4,26]. For lgk=9, accuracy is more than 97% for SHA1 hashed tags.
+    * @return estimator instance
+    */
+  def newEstimator(lgK: Int = 9): CardinalityEstimator = {
+    new CpcEstimator(lgK)
+  }
+
+  private class CpcEstimator(val lgK: Int) extends CardinalityEstimator {
+    private val sketch = new CpcSketch(lgK)
+
+    override def update(obj: AnyRef): Unit = {
+      sketch.update(obj.hashCode())
+    }
+
+    override def cardinality: Int = sketch.getEstimate.toInt
+  }
+}
---
+++ atlas-core/src/test/scala/com/netflix/atlas/core/util/CardinalityEstimatorSuite.scala
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2014-2020 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.netflix.atlas.core.util
+
+import org.scalatest.funsuite.AnyFunSuite
+
+import scala.util.Using
+
+class CardinalityEstimatorSuite extends AnyFunSuite {
+
+  private def check(estimator: CardinalityEstimator, values: Seq[String]): Unit = {
+    var errorSum = 0.0
+    values.zipWithIndex.foreach {
+      case (v, i) =>
+        estimator.update(v)
+        val actual = i + 1
+        val estimate = estimator.cardinality
+        val percentError = 100.0 * math.abs(estimate - actual) / actual
+        errorSum += percentError
+    }
+    val avgPctError = errorSum / values.size
+
+    assert(avgPctError < 5, "error should be less than 5%")
+  }
+
+  test("estimate sha1 string") {
+    val values = Using.resource(Streams.resource("cardinalityEstimator.txt")) { in =>
+      Streams.lines(in).toList
+    }
+
+    check(CardinalityEstimator.newEstimator(), values.slice(0, 1))
+    check(CardinalityEstimator.newEstimator(), values.slice(0, 10))
+    check(CardinalityEstimator.newEstimator(), values.slice(0, 100))
+    check(CardinalityEstimator.newEstimator(), values.slice(0, 200))
+  }
+
+  test("estimate int string") {
+    // verify reasonably accurate estimate with strings that are fairly similar
+    val values = (0 until 1000).map(_.toString)
+
+    check(CardinalityEstimator.newEstimator(), values.slice(0, 1))
+    check(CardinalityEstimator.newEstimator(), values.slice(0, 10))
+    check(CardinalityEstimator.newEstimator(), values.slice(0, 100))
+    check(CardinalityEstimator.newEstimator(), values.slice(0, 200))
+  }
+
+}
