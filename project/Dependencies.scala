e44877803fd578f5c90c22f83a66ef478333d0f4
Brian Harrington <brharrington@gmail.com>
2021-03-03 12:26:29 -0600

    scala 2.13.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
     val guice      = "4.1.0"
     val jackson    = "2.12.1"
     val log4j      = "2.14.0"
-    val scala      = "2.13.4"
+    val scala      = "2.13.5"
     val slf4j      = "1.7.30"
     val spectator  = "0.125.0"
 
