8aca72849ab905adbe11f51bb418f8afba43b1aa
Brian Harrington <brharrington@gmail.com>
2021-10-06 13:57:50 -0500

    datasketches-java 3.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -29,7 +29,7 @@ object Dependencies {
   val akkaStreamTestkit = "com.typesafe.akka" %% "akka-stream-testkit" % akka
   val akkaTestkit       = "com.typesafe.akka" %% "akka-testkit" % akka
   val caffeine          = "com.github.ben-manes.caffeine" % "caffeine" % "2.9.2"
-  val datasketches      = "org.apache.datasketches" % "datasketches-java" % "2.0.0"
+  val datasketches      = "org.apache.datasketches" % "datasketches-java" % "3.0.0"
   val equalsVerifier    = "nl.jqno.equalsverifier" % "equalsverifier" % "3.7.1"
   val frigga            = "com.netflix.frigga" % "frigga" % "0.25.0"
   val guiceCoreBase     = "com.google.inject" % "guice"
