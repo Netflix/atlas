8c26602e58c4567bac094029e1dd62071a159782
Brian Harrington <brharrington@gmail.com>
2022-01-03 10:36:34 -0600

    akka 2.6.18

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ import sbt.librarymanagement.DependencyBuilders.OrganizationArtifactName
 
 object Dependencies {
   object Versions {
-    val akka       = "2.6.17"
+    val akka       = "2.6.18"
     val akkaHttpV  = "10.2.7"
     val iep        = "3.0.10"
     val guice      = "5.0.1"
