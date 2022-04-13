114cedf8beb7a69bc5c00c2f632a5cc365d45769
Brian Harrington <brharrington@gmail.com>
2022-04-13 10:47:12 -0500

    scala-collection-compat 2.7.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -60,7 +60,7 @@ object Dependencies {
   val postgres          = "org.postgresql" % "postgresql" % "42.3.3"
   val postgresEmbedded  = "io.zonky.test" % "embedded-postgres" % "1.3.1"
   val roaringBitmap     = "org.roaringbitmap" % "RoaringBitmap" % "0.9.25"
-  val scalaCompat       = "org.scala-lang.modules" %% "scala-collection-compat" % "2.6.0"
+  val scalaCompat       = "org.scala-lang.modules" %% "scala-collection-compat" % "2.7.0"
   val scalaCompiler     = "org.scala-lang" % "scala-compiler"
   val scalaLibrary      = "org.scala-lang" % "scala-library"
   val scalaLibraryAll   = "org.scala-lang" % "scala-library-all"
