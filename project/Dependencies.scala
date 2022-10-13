9deca0c6f4553520bbbd2b46319ad08e7713ae82
Brian Harrington <brharrington@gmail.com>
2022-10-13 07:56:42 -0500

    scala 2.13.10

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies {
     val iep        = "4.0.0"
     val jackson    = "2.13.3"
     val log4j      = "2.18.0"
-    val scala      = "2.13.8"
+    val scala      = "2.13.10"
     val slf4j      = "1.7.36"
     val spectator  = "1.3.8"
     val spring     = "5.3.22"
