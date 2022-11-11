6007d243460737a4b4a90b50afcfa9afdce49278
Brian Harrington <brharrington@gmail.com>
2022-11-11 17:58:37 -0600

    lwc: use SortedTagMap for data points
    The main advantage is that for eval it can just quickly
    copy into an array without all the hashing and extra
    space in the hash array to avoid collisions.
    
    Note, this change is safe to roll out as long as the
    lwcapi service is pushed first.

---
+++ atlas-eval/src/main/scala/com/netflix/atlas/eval/util/SortedTagMapDeserializer.scala
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2014-2022 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.netflix.atlas.eval.util
+
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.DeserializationContext
+import com.fasterxml.jackson.databind.JsonDeserializer
+import com.netflix.atlas.core.util.SortedTagMap
+
+/**
+  * Custom deserializer for tag maps to go directly to `SortedTagMap` type. It is assumed
+  * that each tag map should have a relatively small number of entries.
+  *
+  * @param initSize
+  *     The initial size to use for the maps.
+  */
+class SortedTagMapDeserializer(initSize: Int) extends JsonDeserializer[SortedTagMap] {
+
+  /** Default constructor so it can be used with the annotations. */
+  def this() = this(10)
+
+  override def deserialize(p: JsonParser, ctxt: DeserializationContext): SortedTagMap = {
+    val builder = SortedTagMap.builder(initSize)
+    var k = p.nextFieldName()
+    while (k != null) {
+      val v = p.nextTextValue()
+      builder.add(k, v)
+      k = p.nextFieldName()
+    }
+    builder.result()
+  }
+}
