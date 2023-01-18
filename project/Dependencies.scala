45e03a20d5a41bc529836c69149a7093e71fb93e
Brian Harrington <brharrington@gmail.com>
2023-01-18 11:55:07 -0600

    spring 5.3.25

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,7 +19,7 @@ object Dependencies {
     val scala      = "2.13.10"
     val slf4j      = "1.7.36"
     val spectator  = "1.5.2"
-    val spring     = "5.3.24"
+    val spring     = "5.3.25"
 
     val crossScala = Seq(scala)
   }
