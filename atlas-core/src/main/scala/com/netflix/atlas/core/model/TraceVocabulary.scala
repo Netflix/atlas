ee7e066873d1cff9fde0988d6ec959ded75938f2
brharrington <brharrington@gmail.com>
2023-04-18 21:23:51 -0500

    lwc: add stack lang operators for events (#1543)
    Adds vocabularies for event and trace queries that will be
    used via LWC.

---
+++ atlas-core/src/main/scala/com/netflix/atlas/core/model/TraceVocabulary.scala
@@ -0,0 +1,129 @@
+/*
+ * Copyright 2014-2023 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.netflix.atlas.core.model
+
+import com.netflix.atlas.core.stacklang.SimpleWord
+import com.netflix.atlas.core.stacklang.Vocabulary
+import com.netflix.atlas.core.stacklang.Word
+
+object TraceVocabulary extends Vocabulary {
+
+  import ModelExtractors._
+
+  val name: String = "trace"
+
+  val dependsOn: List[Vocabulary] = List(QueryVocabulary)
+
+  override def words: List[Word] = List(
+    SpanAndWord,
+    SpanOrWord,
+    SpanFilterWord,
+    ChildWord
+  )
+
+  case object SpanAndWord extends SimpleWord {
+
+    override def name: String = "span-and"
+
+    override protected def matcher: PartialFunction[List[Any], Boolean] = {
+      case TraceQueryType(_) :: TraceQueryType(_) :: _ => true
+    }
+
+    override protected def executor: PartialFunction[List[Any], List[Any]] = {
+      case TraceQueryType(q2) :: TraceQueryType(q1) :: stack =>
+        TraceQuery.SpanAnd(q1, q2) :: stack
+    }
+
+    override def signature: String = "q1:TraceQuery q2:TraceQuery -- TraceQuery"
+
+    override def summary: String =
+      """
+        |Matches if the trace has a span that matches `q1` and a span that matches `q2`.
+        |""".stripMargin
+
+    override def examples: List[String] = List("app,foo,:eq,app,bar,:eq")
+  }
+
+  case object SpanOrWord extends SimpleWord {
+
+    override def name: String = "span-or"
+
+    override protected def matcher: PartialFunction[List[Any], Boolean] = {
+      case TraceQueryType(_) :: TraceQueryType(_) :: _ => true
+    }
+
+    override protected def executor: PartialFunction[List[Any], List[Any]] = {
+      case TraceQueryType(q2) :: TraceQueryType(q1) :: stack =>
+        TraceQuery.SpanOr(q1, q2) :: stack
+    }
+
+    override def signature: String = "q1:TraceQuery q2:TraceQuery -- TraceQuery"
+
+    override def summary: String =
+      """
+        |Matches if the trace has a span that matches `q1` or a span that matches `q2`.
+        |""".stripMargin
+
+    override def examples: List[String] = List("app,foo,:eq,app,bar,:eq")
+  }
+
+  case object ChildWord extends SimpleWord {
+
+    override def name: String = "child"
+
+    override protected def matcher: PartialFunction[List[Any], Boolean] = {
+      case TraceQueryType(_) :: TraceQueryType(_) :: _ => true
+    }
+
+    override protected def executor: PartialFunction[List[Any], List[Any]] = {
+      case TraceQueryType(q2) :: TraceQueryType(q1) :: stack =>
+        TraceQuery.Child(q1, q2) :: stack
+    }
+
+    override def signature: String = "q1:TraceQuery q2:TraceQuery -- TraceQuery"
+
+    override def summary: String =
+      """
+        |Matches if the trace has a span that matches `q1` with a direct child span that
+        |matches `q2`.
+        |""".stripMargin
+
+    override def examples: List[String] = List("app,foo,:eq,app,bar,:eq")
+  }
+
+  case object SpanFilterWord extends SimpleWord {
+
+    override def name: String = "span-filter"
+
+    override protected def matcher: PartialFunction[List[Any], Boolean] = {
+      case (_: Query) :: TraceQueryType(_) :: _ => true
+    }
+
+    override protected def executor: PartialFunction[List[Any], List[Any]] = {
+      case (f: Query) :: TraceQueryType(q) :: stack =>
+        TraceQuery.SpanFilter(q, f) :: stack
+    }
+
+    override def signature: String = "q:TraceQuery f:Query -- SpanFilter"
+
+    override def summary: String =
+      """
+        |Filter to select the set of spans from a trace to forward as events.
+        |""".stripMargin
+
+    override def examples: List[String] = List("app,foo,:eq,app,bar,:eq")
+  }
+}
---
+++ atlas-core/src/test/scala/com/netflix/atlas/core/model/EventVocabularySuite.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2014-2023 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.netflix.atlas.core.model
+
+import com.netflix.atlas.core.stacklang.Interpreter
+import munit.FunSuite
+
+class EventVocabularySuite extends FunSuite {
+
+  private val interpreter = new Interpreter(EventVocabulary.allWords)
+
+  private def parse(str: String): EventExpr.Table = {
+    interpreter.execute(str).stack match {
+      case (t: EventExpr.Table) :: Nil => t
+      case _                           => throw new IllegalArgumentException(str)
+    }
+  }
+
+  test("table, empty set of columns") {
+    val e = intercept[IllegalArgumentException] {
+      parse("name,sps,:eq,(,),:table")
+    }
+    assert(e.getMessage.contains("set of columns cannot be empty"))
+  }
+
+  test("table, two columns") {
+    val table = parse("name,sps,:eq,(,a,b,),:table")
+    assertEquals(table.columns, List("a", "b"))
+    assertEquals(table.query, Query.Equal("name", "sps"))
+  }
+}
