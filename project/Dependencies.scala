191faa78630d5f2ac242e1a3364bf070a5d3dce0
Brian Harrington <brharrington@gmail.com>
2023-10-13 08:30:14 -0500

    roaringbitmap 1.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -53,7 +53,7 @@ object Dependencies {
   val munit             = "org.scalameta" %% "munit" % "0.7.29"
   val postgres          = "org.postgresql" % "postgresql" % "42.6.0"
   val postgresEmbedded  = "io.zonky.test" % "embedded-postgres" % "2.0.4"
-  val roaringBitmap     = "org.roaringbitmap" % "RoaringBitmap" % "0.9.48"
+  val roaringBitmap     = "org.roaringbitmap" % "RoaringBitmap" % "1.0.0"
   val scalaCompat       = "org.scala-lang.modules" %% "scala-collection-compat" % "2.11.0"
   val scalaCompatJdk8   = "org.scala-lang.modules" %% "scala-java8-compat" % "1.0.2"
   val scalaCompiler     = "org.scala-lang" % "scala-compiler"
