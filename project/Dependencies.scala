b6adef40dcd21bb46ff5fe1258579699faf2abeb
Brian Harrington <brharrington@gmail.com>
2024-04-04 10:48:54 -0500

    jackson 2.17.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
     val pekko       = "1.0.2"
     val pekkoHttpV  = "1.0.1"
     val iep        = "5.0.19"
-    val jackson    = "2.16.1"
+    val jackson    = "2.17.0"
     val log4j      = "2.23.0"
     val scala      = "2.13.13"
     val slf4j      = "1.7.36"
