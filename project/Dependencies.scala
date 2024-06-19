8ac16b05e03b94df279ecc44ac17c4a798ea760b
Brian Harrington <brharrington@gmail.com>
2024-06-19 16:59:25 -0500

    iep 5.0.23

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   object Versions {
     val pekko       = "1.0.2"
     val pekkoHttpV  = "1.0.1"
-    val iep        = "5.0.22"
+    val iep        = "5.0.23"
     val jackson    = "2.17.1"
     val log4j      = "2.23.1"
     val scala      = "2.13.14"
