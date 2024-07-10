e354bc04d90c9c4e5ec20c37faf2220288368279
Brian Harrington <brharrington@gmail.com>
2024-07-10 10:35:59 -0500

    lwc-events: add pre-filter method for events
    Add `couldMatch` method to the client to allow for filtering
    our some data early if it requires an expensive transform to
    generate the final event.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
     val log4j      = "2.23.1"
     val scala      = "2.13.14"
     val slf4j      = "1.7.36"
-    val spectator  = "1.7.14"
+    val spectator  = "1.7.16"
     val spring     = "6.1.10"
 
     val crossScala = Seq(scala, "3.4.1")
