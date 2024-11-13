9a9ebdd4e98b823a96b666558d3cf4b0b415a709
Brian Harrington <brharrington@gmail.com>
2024-11-13 16:05:52 -0600

    postgresql 42.7.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -52,7 +52,7 @@ object Dependencies {
   val log4jJul          = "org.apache.logging.log4j" % "log4j-jul" % log4j
   val log4jSlf4j        = "org.apache.logging.log4j" % "log4j-slf4j-impl" % log4j
   val munit             = "org.scalameta" %% "munit" % "1.0.2"
-  val postgres          = "org.postgresql" % "postgresql" % "42.7.3"
+  val postgres          = "org.postgresql" % "postgresql" % "42.7.4"
   val postgresEmbedded  = "io.zonky.test" % "embedded-postgres" % "2.0.7"
   val roaringBitmap     = "org.roaringbitmap" % "RoaringBitmap" % "1.3.0"
   val scalaCompat       = "org.scala-lang.modules" %% "scala-collection-compat" % "2.12.0"
