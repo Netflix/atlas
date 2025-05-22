d69c9163f34b78d56bc40794da89aea82a2f7303
Brian Harrington <brharrington@gmail.com>
2025-05-22 11:25:41 -0500

    core: update sed exprs to support tag vars
    Updates the sed expressions for the legend to fallback to
    the tags if the variable is not present on the regex matcher.
    Since the substitution is outside the matcher, escape before
    appending to avoid spurious exceptions if using a `$` or `\`
    in the replacement value.

--- atlas-core/src/test/scala/com/netflix/atlas/core/model/StyleExprSuite.scala
+++ atlas-core/src/test/scala/com/netflix/atlas/core/model/StyleExprSuite.scala
@@ -67,15 +67,22 @@ class StyleExprSuite extends FunSuite {
   private def check(sed: String, expected: String): Unit = {
     test(sed) {
       val expr = newExpr(s"$$b", sed)
-      val ts = newTimeSeries("foo", Map("a" -> "1", "b" -> "one_21_25_26_3F"))
+      val ts = newTimeSeries("foo", Map("a" -> "1", "2" -> "two", "b" -> "one_21_25_26_3F"))
       assertEquals(expr.legend(ts), expected)
     }
   }
 
   check("^([a-z]+).*$,prefix [$1],:s", "prefix [one]")
   check("^(?<prefix>[a-z]+).*$,prefix [$prefix],:s", "prefix [one]")
+  check("^(?<prefix>[A-Z]*).*$,prefix [$prefix],:s", "prefix []")
+  check("^(?<prefix>[a-z]+).*$,prefix [$1],:s", "prefix [one]")
+  check("^(?<prefix>[A-Z]*).*$,prefix [$1],:s", "prefix []")
+  check("^(?<prefix>[A-Z]*).*$,prefix [$2],:s", "prefix [two]")
   check("_.*,,:s", "one")
   check("(_[A-F0-9]{2}), $1,:s,hex,:decode", "one ! % & ?")
+  check("^([a-z]+).*$,$()prefix [$1],:s", "$prefix [one]")
+  check("^([a-z]+).*$,\\prefix [$1],:s", "\\prefix [one]")
+  check("^([a-z]+).*$,$a [$1],:s", "1 [one]")
   check("hex,:decode,(_[A-F0-9]{2}), $1,:s", "one!%&?")
   check("hex,:decode,%,_25,:s", "one!_25&?")
   check("hex,:decode,%,_25,:s,hex,:decode", "one!%&?")
