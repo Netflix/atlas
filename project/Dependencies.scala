3cbfc6fa2e039174b40185b41e7b3568ed214965
Brian Harrington <brharrington@gmail.com>
2026-02-06 12:23:27 -0600

    embedded-postgres 2.2.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -55,7 +55,7 @@ object Dependencies {
   val otelSdk           = "io.opentelemetry" % "opentelemetry-sdk" % otel
   val otelSdkTest       = "io.opentelemetry" % "opentelemetry-sdk-testing" % otel
   val postgres          = "org.postgresql" % "postgresql" % "42.7.8"
-  val postgresEmbedded  = "io.zonky.test" % "embedded-postgres" % "2.1.1"
+  val postgresEmbedded  = "io.zonky.test" % "embedded-postgres" % "2.2.0"
   val roaringBitmap     = "org.roaringbitmap" % "RoaringBitmap" % "1.3.0"
   val scalaCompat       = "org.scala-lang.modules" %% "scala-collection-compat" % "2.14.0"
   val scalaCompatJdk8   = "org.scala-lang.modules" %% "scala-java8-compat" % "1.0.2"
