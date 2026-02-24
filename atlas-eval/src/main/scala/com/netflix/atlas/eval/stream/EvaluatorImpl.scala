29c1bb0be967aa9f74cbfe41f0b00193c14abbc7
Brian Harrington <brharrington@gmail.com>
2026-02-23 22:00:23 -0600

    fix formatting

--- atlas-eval/src/main/scala/com/netflix/atlas/eval/stream/EvaluatorImpl.scala
+++ atlas-eval/src/main/scala/com/netflix/atlas/eval/stream/EvaluatorImpl.scala
@@ -525,7 +525,7 @@ private[stream] abstract class EvaluatorImpl(
       client = instance => createWebSocketFlow(instance),
       sizeOf = bytes => bytes.length.toLong,
       targetRateBytesPerSec = broadcastTargetRate,
-      registry = registry,
+      registry = registry
     )
   }
 
