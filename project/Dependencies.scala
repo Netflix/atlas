b9a6236125c5ff41b7167ea0128ec0f2ce4f08f3
Brian Harrington <brharrington@gmail.com>
2026-02-27 07:36:24 -0600

    core: tag index optimizations
    RoaringBitmap library is published to maven again, bump to
    the latest version to pick up recent improvemnts. Also use
    helper methods for intersection and andNot rather than the
    custom methods.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -53,7 +53,7 @@ object Dependencies {
   val otelSdkTest       = "io.opentelemetry" % "opentelemetry-sdk-testing" % otel
   val postgres          = "org.postgresql" % "postgresql" % "42.7.9"
   val postgresEmbedded  = "io.zonky.test" % "embedded-postgres" % "2.2.0"
-  val roaringBitmap     = "org.roaringbitmap" % "RoaringBitmap" % "1.3.0"
+  val roaringBitmap     = "org.roaringbitmap" % "RoaringBitmap" % "1.6.12"
   val scalaCompat       = "org.scala-lang.modules" %% "scala-collection-compat" % "2.14.0"
   val scalaCompatJdk8   = "org.scala-lang.modules" %% "scala-java8-compat" % "1.0.2"
   val scalaCompiler     = "org.scala-lang" % "scala-compiler"
