3735d31f3fd9c4a3be3a2c3f89791f90963b8ea3
Brian Harrington <brharrington@gmail.com>
2026-06-01 21:01:07 -0500

    iep 6.0.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   object Versions {
     val pekko       = "1.6.0"
     val pekkoHttpV  = "1.3.0"
-    val iep         = "6.0.2"
+    val iep         = "6.0.3"
     val jackson     = "3.1.1"
     val log4j       = "2.25.4"
     val otel        = "1.60.1"
