8155330f4fd1a3df797f50b2d1f0146ef39893ff
Brian Harrington <brharrington@gmail.com>
2026-06-01 21:24:12 -0500

    log4j 2.26.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     val pekkoHttpV  = "1.3.0"
     val iep         = "6.0.3"
     val jackson     = "3.1.4"
-    val log4j       = "2.25.4"
+    val log4j       = "2.26.0"
     val otel        = "1.60.1"
     val scala       = "2.13.18"
     val slf4j       = "2.0.17"
