7e6e09601f2eae35bb23a3ce2a8eb5009cc4420d
Brian Harrington <brharrington@gmail.com>
2026-08-05 10:09:37 -0500

    pekko-http 1.4.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ import sbt.*
 object Dependencies {
   object Versions {
     val pekko       = "1.6.0"
-    val pekkoHttpV  = "1.3.0"
+    val pekkoHttpV  = "1.4.0"
     val iep         = "6.0.7"
     val jackson     = "3.2.1"
     val log4j       = "2.26.1"
